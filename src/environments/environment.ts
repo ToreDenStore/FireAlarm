90182e76f0d2f1f2f99afe68aef0ca0731ccdf6e
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-11-24 18:29:41 +0100

    Add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBI7821u2-zTd8prHPj1PsuVUQQnuijiMQ',
+    authDomain: 'firealarm-7f360.firebaseapp.com',
+    databaseURL: 'https://firealarm-7f360.firebaseio.com',
+    projectId: 'firealarm-7f360',
+    storageBucket: 'firealarm-7f360.appspot.com',
+    messagingSenderId: '621219891899',
+    appId: '1:621219891899:web:09ad8e91db08c1110c379e',
+    measurementId: 'G-8W06W50JDV'
+  }
 };
 
 /*
